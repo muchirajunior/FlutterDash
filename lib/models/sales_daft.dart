5b51dab9fa76de4b4ca5a1536abee1bc1c02f5aa
muchirajunior <muchirajunior022@gmail.com>
2024-02-10 11:47:45 +0300

    sales draft model

--- lib/models/sales_daft.dart
+++ lib/models/sales_daft.dart
@@ -2,12 +2,46 @@ class SalesDraft{
   int? id;
   String? title;
   List<SalesDraftItem> items =[];
+
+  SalesDraft.fromJson(Map json){
+    id = json['id'];
+    title = json['title'];
+    items = _getItems(json['items']);
+  }
+
+  Map toJson()=>{
+    'id': id,
+    'title': title,
+    'items': items.map((item) => item.toJson()).toList()
+  };
+
+  List<SalesDraftItem> _getItems(List json){
+    List<SalesDraftItem> itms =[];
+    for (var item in json) {
+      items.add(SalesDraftItem.fromJson(item));
+    }
+    return itms;
+  }
   
 }
 
 class SalesDraftItem{
-  String? item;
+  String? name;
   String? code;
   double? price;
   int? quantity;
+
+  SalesDraftItem.fromJson(Map json){
+    name = json['name'];
+    code = json['code'];
+    price = json['price'];
+    quantity = json['quantity'];    
+  }
+
+  Map toJson()=>{
+    'name': name,
+    'code': code,
+    'price': price,
+    'quantity': quantity
+  };
 }
